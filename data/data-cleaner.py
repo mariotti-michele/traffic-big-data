6053443249a95dcbe0b93a1d4e2d605b93bced10
mariotti-michele <152858107+mariotti-michele@users.noreply.github.com>
2025-05-07 19:33:16 +0200

    data cleaning

---
+++ data/data-cleaner.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import os
+
+file_csv = [os.path.join("data", f) for f in os.listdir("data") if f.endswith('.csv')]
+
+column_to_keep = ["Giorno", "Postazione", "Strada", "Transiti - Totale"]
+
+df_joint = pd.concat(
+    [pd.read_csv(f, sep=';', quotechar='"', encoding='utf-8') for f in file_csv],
+    ignore_index=True
+)
+
+df_joint = df_joint[column_to_keep]
+
+df_joint["Transiti - Totale"] = (
+    df_joint["Transiti - Totale"]
+    .astype(str)
+    .str.replace('.', '', regex=False)
+    .replace('', pd.NA)
+    .astype(float)
+    .fillna(0)
+    .astype(int)
+)
+
+df_joint["Postazione"] = (
+    df_joint["Postazione"]
+    .fillna(0)
+    .astype(float)
+    .astype(int)
+)
+
+df_joint = df_joint[df_joint["Transiti - Totale"] != 0]
+
+df_joint.to_csv("traffic-dataset.csv", index=False)
