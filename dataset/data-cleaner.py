ea9482ab8c49d7a827826a709a530d92352a2bb9
JonyBaba01 <martin.arsoski@studenti.unipg.it>
2025-05-11 15:21:04 +0200

    ordinati i dataset in base alla data

--- dataset/data-cleaner.py
+++ dataset/data-cleaner.py
@@ -31,6 +31,8 @@ df_joint["Postazione"] = (
 
 df_joint = df_joint[df_joint["Transiti - Totale"] != 0]
 
+df_joint["Giorno"] = pd.to_datetime(df_joint["Giorno"], dayfirst=True, errors="coerce")
+
 postazioni_uniche = sorted(df_joint["Postazione"].unique())
 metà = len(postazioni_uniche) // 2
 postazioni_A = set(postazioni_uniche[:metà])
@@ -41,6 +43,9 @@ df_B = df_joint[df_joint["Postazione"].isin(postazioni_B)]
 
 df_A = df_A.sort_values(by=["Giorno", "Postazione"])
 df_B = df_B.sort_values(by=["Giorno", "Postazione"])
+
+df_A["Giorno"] = df_A["Giorno"].dt.strftime("%d/%m/%Y")
+df_B["Giorno"] = df_B["Giorno"].dt.strftime("%d/%m/%Y")
 
 df_A.to_csv(os.path.join("dataset", "traffic-dataset-half-A.csv"), index=False)
 df_B.to_csv(os.path.join("dataset", "traffic-dataset-half-B.csv"), index=False)
