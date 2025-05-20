3b5b4cfa6f049a111795e8dbf294ff3f8391cb1c
mariotti-michele <152858107+mariotti-michele@users.noreply.github.com>
2025-05-20 11:36:02 +0200

    aggiornato startWorkersAndSparkProcessor.sh

--- scripts/startWorkersAndSparkProcessor.sh
+++ scripts/startWorkersAndSparkProcessor.sh
@@ -4,7 +4,9 @@ ssh diabd@192.168.56.103 'bash -s' < ./startWorker2.sh &
 
 ssh diabd@192.168.56.102 'bash -s' < ./startWorker1.sh &
 
+runSparkProcessor.sh &
+
 wait
 
-echo "[INFO] Operazioni completate su entrambi i Worker."
+echo "Operazioni completate su entrambi i Worker."
 sleep 10
