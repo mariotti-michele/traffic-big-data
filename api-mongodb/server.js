de6d8fb4649517a0785a94643e51aea5b31b9dbd
mariotti-michele <152858107+mariotti-michele@users.noreply.github.com>
2025-05-21 16:00:11 +0200

    modifiche server api

--- api-mongodb/server.js
+++ api-mongodb/server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+
+const { MongoClient, ReadConcern, ReadPreference } = require("mongodb");
+
+const uri = "mongodb://192.168.56.101,192.168.56.102,192.168.56.103";
+const client = new MongoClient(uri, {
+    replicaSet: "rstraffic",
+    readConcern: new ReadConcern("local"),
+    readPreference: ReadPreference.primaryPreferred,
+});
+
 const app = express();
 const PORT = 3000;
 
-const uri = "mongodb://localhost:27017";
-const client = new MongoClient(uri);
 const dbName = "traffic";
 const transitsCollectionName = "daily_transits";
 const statsCollectionName = "station_stats";
@@ -13,17 +20,14 @@ const statsCollectionName = "station_stats";
 app.use(cors());
 app.use(express.static("public"));
 
+let db;
+
 app.get("/station/:id", async (req, res) => {
     const { id } = req.params;
 
     try {
-        await client.connect();
-        const db = client.db(dbName);
-        const transitsCollection = db.collection(transitsCollectionName);
-        const statsCollection = db.collection(statsCollectionName);
-
-        const transits = await transitsCollection.find({ station_id: id }).toArray();
-        const stats = await statsCollection.find({ station_id: id }).toArray();
+        const transits = await db.collection(transitsCollectionName).find({ station_id: id }).toArray();
+        const stats = await db.collection(statsCollectionName).find({ station_id: id }).toArray();
 
         res.json({
             station_id: id,
@@ -39,11 +43,7 @@ app.get("/station/:id", async (req, res) => {
 
 app.get("/station_ids", async (req, res) => {
     try {
-        await client.connect();
-        const db = client.db(dbName);
-        const transitsCollection = db.collection(transitsCollectionName);
-
-        const ids = await transitsCollection.distinct("station_id");
+        const ids = await db.collection(transitsCollectionName).distinct("station_id");
         res.json(ids);
     } catch (err) {
         console.error(err);
@@ -51,7 +51,18 @@ app.get("/station_ids", async (req, res) => {
     }
 });
 
+async function main() {
+    await client.connect();
+    db = client.db(dbName);
+    app.listen(PORT, () => {
+        console.log(`Server in ascolto su http://localhost:${PORT}`);
+    });
+}
 
-app.listen(PORT, () => {
-    console.log(`Server in ascolto su http://localhost:${PORT}`);
+process.on("SIGINT", async () => {
+    console.log("Chiusura connessione MongoDB...");
+    await client.close();
+    process.exit(0);
 });
+
+main().catch(console.error);
